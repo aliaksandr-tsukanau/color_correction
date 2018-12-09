38cc5f15a2e776db735d62671da5c30a9587deb9
sasha <sasha.ts91@gmail.com>
2018-12-09 20:13:45 +0300

    get_grid_by_name endpoint

--- run_backend.py
+++ run_backend.py
@@ -1,4 +1,8 @@
-from flask import Flask
+import json
+import bson.json_util
+
+from flask import Flask, request, make_response
+from flask.json import jsonify
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
@@ -7,9 +11,14 @@ app.config['MONGO_URI'] = 'mongodb://localhost:27017/color_correction'
 mongo = PyMongo(app)
 
 
-@app.route('/')
+@app.route('/get_grid_by_name')
 def root():
-    return 'Hello'
+    name = request.args['name']
+    grid = mongo.db.grids.find_one({'name': name})
+    response_text = bson.json_util.dumps(grid)
+    resp = make_response(response_text)
+    resp.mimetype = 'application/json'
+    return resp
 
 
 if __name__ == '__main__':
