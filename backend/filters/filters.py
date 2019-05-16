ad43337a9ece3b8fd813e98c6b3f40f851118e38
sasha <a-tsukanov@protonmail.com>
2019-05-16 20:53:42 +0300

    auto filter icons

--- backend/filters/filters.py
+++ backend/filters/filters.py
@@ -9,7 +9,9 @@ import os
 from flask.json import jsonify
 from flask import Blueprint, request, send_from_directory
 
+from backend.processing.processing import PALETTE
 from db.client_instance import DB_CLIENT
+from image.image import process_img_with_lut, read_initial_rgb, initial_to_lab, save_processed_image
 
 filter_requests = Blueprint('filter_requests', __name__)
 
@@ -22,10 +24,24 @@ def all_filters():
 
 @filter_requests.route('/get_filter_img_by_name')
 def get_filter_img():
-    name = request.args['name'] + '.jpeg'
+    orig_name = request.args['name']
+    name = orig_name + '.jpeg'
     path = os.path.join(os.getcwd(), 'files')
     path = os.path.join(path, 'filter_icons')
-    return send_from_directory(
-        path,
-        name
-    )
+    if os.path.exists(os.path.join(path, name)):
+        return send_from_directory(
+            path,
+            name
+        )
+    else:
+        grid = DB_CLIENT.get_grid_obj(orig_name)
+        initial_path = os.path.join(os.getcwd(), 'files')
+        initial_path = os.path.join(initial_path, 'filter_base.jpeg')
+        initial = read_initial_rgb(initial_path)
+        initial_lab = initial_to_lab(initial)
+        icon = process_img_with_lut(initial_lab, PALETTE, grid, False)
+        save_processed_image(icon, os.path.join(path, name))
+        return send_from_directory(
+            path,
+            name
+        )
